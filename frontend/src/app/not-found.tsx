67f7fa75ce34a7ba0b5a016bc7fd10f4c807415d
GeroKramar <gerokramar@gmail.com>
2024-05-29 15:10:15 -0300

    pagina 404

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -4,24 +4,31 @@ import React from 'react'
 const NotfoundPage = () => {
   return (
     <>
-
-<div className="p-10 bg-gray-100 flex items-center">
-	<div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
-   		<div className="max-w-md">
-      		<div className="text-5xl font-dark font-bold">404</div>
-            <p
-              className="text-2xl md:text-3xl font-light leading-normal"
-            >Sorry we couldn't find this page. </p>
-          <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-          <Link href="/">
-          <button className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-gray-700 active:bg-gray-700 hover:bg-gray-800">back to homepage</button>
-          
-          </Link>
+<style>@import url(https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css);</style>
+<div className="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative">
+    <div className="flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
+        <div className="w-full md:w-1/2">
+            <div className="mb-10 md:mb-20 text-gray-600 font-light">
+                <h1 className="font-black uppercase text-3xl lg:text-5xl text-yellow-500 mb-10">No encontramos lo que buscas!</h1>
+                <p>La pagina que estas buscando no existe</p>
+                <p>Intenta buscarla nuevamente o puedes volver al inicio.</p>
+            </div>
+            <div className="mb-20 md:mb-0">
+                <button className="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-yellow-500 hover:text-yellow-600"><i className="mdi mdi-arrow-left mr-2"></i>Volver al inicio</button>
+            </div>
+        </div>
+        <div className="w-full md:w-1/2 text-center">
+        <img src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" alt="" />
+        </div>
     </div>
-      
-    
-  </div>
+    <div className="w-64 md:w-96 h-96 md:h-full bg-blue-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"></div>
+    <div className="w-96 h-full bg-yellow-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"></div>
 </div>
+
+
+
+
+
     </>
   )
 }
