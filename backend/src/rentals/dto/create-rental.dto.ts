584ff63393a44ba659cf4f4b58e9516b6e51d48a
Danilo Maccari <maccari78@gmail.com>
2024-05-29 20:05:06 -0300

    users & rentals entities & dtos

--- backend/src/rentals/dto/create-rental.dto.ts
+++ backend/src/rentals/dto/create-rental.dto.ts
@@ -1,1 +1,20 @@
-export class CreateRentalDto {}
+import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateRentalDto {
+  @IsDate()
+  @IsNotEmpty()
+  rentalStartDate: Date;
+
+  @IsDate()
+  @IsNotEmpty()
+  rentalEndDate: Date;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  carId: number;
+}
+
--- backend/src/rentals/entities/rental.entity.ts
+++ backend/src/rentals/entities/rental.entity.ts
@@ -1,1 +1,28 @@
-export class Rental {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Car } from '../../cars/entities/car.entity';
+
+@Entity('rentals')
+export class Rental {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'timestamp' })
+  rentalStartDate: Date;
+
+  @Column({ type: 'timestamp' })
+  rentalEndDate: Date;
+
+  @ManyToOne(() => User, user => user.rentals)
+  user: User;
+
+  @ManyToOne(() => Car, car => car.rentals)
+  car: Car;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
+
