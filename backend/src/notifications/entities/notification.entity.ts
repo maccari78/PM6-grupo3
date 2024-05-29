af3667c4c677a78b72a9d95c52f3df462510e1d5
AlvarezMar <alvarez.majuan@hotmail.com>
2024-05-29 17:12:30 -0600

    Creación de Entidades de Direcciones y Notificaciones

--- backend/src/notifications/entities/notification.entity.ts
+++ backend/src/notifications/entities/notification.entity.ts
@@ -1,1 +1,34 @@
-export class Notification {}
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'notifications',
+})
+export class Notification {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'text',
+  })
+  message: string;
+
+  @Column({
+    type: 'boolean',
+  })
+  isRead: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User, (user) => user.notifications)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
