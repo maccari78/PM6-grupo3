df516345b7818fdfda4ff2818bd5483dc064f00e
Camila Fabre <camilafabre98@gmail.com>
2024-05-30 10:18:45 -0300

    navBar responsive parte 2

--- frontend/src/components/DropdownNavbar/Dropdown.tsx
+++ frontend/src/components/DropdownNavbar/Dropdown.tsx
@@ -1,13 +1,21 @@
-// components/Dropdown.js
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const Dropdown = () => {
+
+  const router = useRouter();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleLogOut = () => {
+    localStorage.removeItem('userSession')
+    router.push("/login")
+}
 
   return (
     <div className="relative inline-block text-left">
@@ -18,26 +26,33 @@ const Dropdown = () => {
         Mi cuenta
       </button>
       {isOpen && (
-        <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg">
+        <div className="absolute right-0 w-56 mt-2 origin-top-right bg-[#222222] border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg">
           <div className="py-1">
             <Link
-              href="/mi-cuenta"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              href="/user"
+              className="block px-4 py-2 text-sm text-white hover:bg-[#494949] "
             >
               Mi cuenta
             </Link>
             <Link
               href="/publicar-vehiculo"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              className="block px-4 py-2 text-sm text-white hover:bg-[#494949] "
             >
               Publicar mi vehículo
             </Link>
             <Link
-              href="/cerrar-sesion"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              href="/ayuda"
+              className="block px-4 py-2 text-sm text-white hover:bg-[#494949]"
+            >
+              Centro de ayuda
+            </Link>
+            <button
+              type="button"
+              className="block px-4 mt-2 text-start py-2 text-sm w-full text-white hover:bg-[#494949]"
+              onClick={handleLogOut}
             >
               Cerrar sesión
-            </Link>
+            </button>
           </div>
         </div>
       )}
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <footer className="bg-[#222222] p-4 font-sans">
-        <div className="container mx-auto text-center text-gray-200">
+        <div className="container mx-auto text-center text-gray-200 font-sans  text-xs">
           <p>&copy; 2024 You Drive. Todos los derechos reservados.</p>
           <div className="mt-2 space-x-4">
             <a href="#" className="hover:text-white">
