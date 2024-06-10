5cd962a5868da37db2e372bc342c97a9b4025855
Darvin Ricardo Alania Inocencio <152816164+DAlaniaInocencio@users.noreply.github.com>
2024-06-10 12:15:29 -0500

    Envio email luego del pago y agregar pequeños cambios en rentals.service

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -33,12 +33,12 @@ export class PostsController {
     return this.postsService.getPostsServiceId(id);
   }
 
-  @ApiBearerAuth()
+  // @ApiBearerAuth()
   @Post()
-  @UseGuards(TokenGuard)
+  // @UseGuards(TokenGuard)
   @UseInterceptors(FilesInterceptor('file', 5))
-  @UseGuards(RolesGuard)
-  @Roles(Role.User, Role.Admin)
+  // @UseGuards(RolesGuard)
+  // @Roles(Role.User, Role.Admin)
   create(
     @Body() createPostDto: CreatePostDto,
     @Headers('Authorization') headers?: string,
--- backend/src/rentals/rentals.module.ts
+++ backend/src/rentals/rentals.module.ts
@@ -7,10 +7,12 @@ import { User } from '../users/entities/user.entity';
 import { Car } from '../cars/entities/car.entity';
 import { Posts } from 'src/posts/entities/post.entity';
 import { UsersModule } from 'src/users/users.module';
-
+import { NotificationsService } from 'src/notifications/notifications.service';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { MailService } from 'src/mail/mail.service';
 @Module({
-  imports: [TypeOrmModule.forFeature([Rental, User, Car, Posts]), UsersModule],
-  providers: [RentalsService],
+  imports: [TypeOrmModule.forFeature([Rental, User, Car, Posts, Notification]), UsersModule],
+  providers: [RentalsService,NotificationsService,MailService],
   controllers: [RentalsController],
 })
 export class RentalsModule {}
