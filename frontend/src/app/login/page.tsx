641e6fb3610b0c4b40b81a6ff3ace816287bcebc
Geronimo Kramar <gerokramar@gmail.com>
2024-06-11 03:54:47 -0300

    Update page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -109,9 +109,9 @@ const Login = () => {
   };
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+      <div className="min-h-screen bg-[url('/background_register_2.svg')] flex flex-col justify-center sm:py-12">
         <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-          <h1 className="font-bold text-center text-2xl mb-5">
+          <h1 className="font-bold text-center text-white text-2xl mb-5">
             Bienvenido a YouDrive!
           </h1>
           <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
