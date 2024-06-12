c587bf0e203d53c97bf634e171de7b9e24d80197
Camila Fabre <camilafabre98@gmail.com>
2024-06-12 17:16:56 -0300

    chatbot parte 2

--- frontend/src/components/Chatbottt/Chatbottt.tsx
+++ frontend/src/components/Chatbottt/Chatbottt.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { lazy, Suspense, useEffect, useState } from "react";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const ChatBot = lazy(() => import("react-chatbotify"));
 
@@ -23,29 +23,94 @@ const Chatbottt = () => {
     return text;
   }
 
-  const flow = {
-    start: {
-      message:
-        "Hola! Bienvenido a YouDrive. Mi nombre es Pepito, ¿cómo puedo ayudarte?",
-      path: "model_loop",
-    },
-    model_loop: {
-      message: async (params: any) => {
-        return await run(params.userInput, params.streamMessage);
-      },
-      path: "model_loop",
-    },
-  };
+  const helpOptions = ["¿Debo iniciar sesion para alquilar?", "Quiero poner en alquiler mi auto", "Terminos y condiciones", "Desactivar cuenta"]; 
+	const flow = {
+		start: {
+			message: "Hello, I am Tan Jin 👋! Welcome to React ChatBotify, I'm excited that you are using our " +
+				"chatbot 😊!",
+			transition: {duration: 1000},
+			path: "show_options"
+		},
+		show_options: {
+			message: "It looks like you have not set up a conversation flow yet. No worries! Here are a few helpful " +
+				"things you can check out to get started:",
+			options: helpOptions,
+			path: "process_options"
+		},
+		prompt_again: {
+			message: "Do you need any other help?",
+			options: helpOptions,
+			path: "process_options"
+		},
+		unknown_input: {
+			message: "Sorry, I do not understand your message 😢! If you require further assistance, you may click on " +
+				"the Github option and open an issue there or visit our discord.",
+			options: helpOptions,
+			path: "process_options"
+		},
+		process_options: {
+			transition: {duration: 0},
+			chatDisabled: true,
+			path: async (params:any) => {
+				let link = "";
+				switch (params.userInput) {
+				case "Quickstart":
+					link = "https://react-chatbotify.tjtanjin.com/docs/introduction/quickstart/";
+					break;
+				case "API Docs":
+					link = "https://react-chatbotify.tjtanjin.com/docs/api/bot_options";
+					break;
+				case "Examples":
+					link = "https://react-chatbotify.tjtanjin.com/docs/examples/basic_form";
+					break;
+				case "Github":
+					link = "https://github.com/tjtanjin/react-chatbotify/";
+					break;
+				case "Discord":
+					link = "https://discord.gg/6R4DK4G5Zh";
+					break;
+				default:
+					return "unknown_input";
+				}
+				await params.injectMessage("Sit tight! I'll send you right there!");
+				setTimeout(() => {
+					window.open(link);
+				}, 1000)
+				return "repeat"
+			},
+		},
+		repeat: {
+			transition: {duration: 3000},
+			path: "prompt_again"
+		},
+	}
 
   const options = {
     theme: {
       // embedded: true,
       primaryColor: "#222222",
-      secondaryColor: "#C4FF0D",
+      secondaryColor: "#a4cb30",
     },
     chatHistory: {
       storageKey: "example_theming",
     },
+    footer: {
+      text: (
+        <div style={{cursor: "pointer"}} 
+          onClick={() => window.open("https://react-chatbotify.tjtanjin.com")}
+        >
+        </div>
+      ),
+    },
+  tooltip: {
+    mode: "CLOSE",
+    text: "Habla conmigo!",
+  },
+  advance: {
+    useCustomMessages: false,
+    useCustomBotOptions: false,
+    useCustomPaths: false,
+  },
   };
 
   const [isLoaded, setIsLoaded] = useState(false);
