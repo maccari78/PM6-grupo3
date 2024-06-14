66892016b980d7a4712e99f966c404308ef8fdea
Danilo Maccari <maccari78@gmail.com>
2024-06-13 22:04:54 -0300

    working on daysRemaining

--- backend/src/rentals/rentals.service.ts
+++ backend/src/rentals/rentals.service.ts
@@ -1,9 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateRentalDto } from './dto/create-rental.dto';
 import { UpdateRentalDto } from './dto/update-rental.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -28,43 +23,27 @@ export class RentalsService {
     private notificationService: NotificationsService,
     private jwtService: JwtService,
   ) {}
-  async create(createRentalDto: CreateRentalDto, currentUser: string, postId) {
-    const {
-      name,
-      price,
-      description,
-      image_url,
-      rentalStartDate,
-      rentalEndDate,
-      ...rest
-    } = createRentalDto;
+
+  async create(createRentalDto: CreateRentalDto, currentUser: string, postId: string) {
+    const { name, /* price, */ description, image_url, rentalStartDate, rentalEndDate, ...rest } = createRentalDto;
 
     const secret = process.env.JWT_SECRET;
-    const payload: JwtPayload = await this.jwtService.verify(currentUser, {
-      secret,
-    });
+    const payload: JwtPayload = await this.jwtService.verify(currentUser, { secret });
 
-    const rental_user = await this.userRepository.findOne({
-      where: { email: payload.sub },
-    });
-
+    const rental_user = await this.userRepository.findOne({ where: { email: payload.sub } });
     if (!rental_user) throw new NotFoundException('Usuario no encontrado');
 
     const findPost = await this.postRepository.findOne({
       where: { id: postId },
       relations: ['car', 'user'],
     });
-    if (!findPost)
-      throw new NotFoundException('Publicación no encontrada en la BD');
-    if (findPost.user.email === rental_user.email)
-      throw new BadRequestException('No puedes alquilar tu propio vehiculo');
 
-    const findCar = await this.carRepository.findOne({
-      where: { id: findPost.car.id },
-    });
+    if (!findPost) throw new NotFoundException('Publicación no encontrada en la BD');
+    if (findPost.user.email === rental_user.email) throw new BadRequestException('No puedes alquilar tu propio vehiculo');
+
+    const findCar = await this.carRepository.findOne({ where: { id: findPost.car.id } });
     if (!findCar) throw new NotFoundException('Vehiculo no encontrado');
-    if (findCar.availability === false)
-      throw new BadRequestException('El vehiculo ya se encuentra alquilado');
+    if (findCar.availability === false) throw new BadRequestException('El vehiculo ya se encuentra alquilado');
 
     // Calcular la duración del alquiler en días
     const startDate = new Date(rentalStartDate);
@@ -75,37 +54,19 @@ export class RentalsService {
     // Calcular el costo total
     const totalCost = dayDiff * findPost.price;
     const room_id = findPost.id + rental_user.id;
-    const newRental = this.rentalRepository.create({
-      rentalStartDate,
-      rentalEndDate,
-      totalCost,
-      room_id,
-      users: [rental_user, findPost.user],
-      posts: findPost,
-      ...rest,
-    });
+    const newRental = this.rentalRepository.create({ rentalStartDate, rentalEndDate, totalCost, room_id, users: [rental_user, findPost.user],
+      posts: findPost, daysRemaining: dayDiff, ...rest, });
 
     const rental = await this.rentalRepository.save(newRental);
-    if (!rental)
-      throw new BadRequestException(
-        'Error al crear el contrato, verifique las relaciones con otras entidades',
-      );
+    if (!rental) throw new BadRequestException( 'Error al crear el contrato, verifique las relaciones con otras entidades' );
 
-    const payment: Payment = {
-      name: name,
-      price: totalCost,
-      description: description,
-      image_url,
-    };
+    const payment: Payment = { name: name, price: totalCost, description: description, image_url };
 
     const url = await this.payment(payment, rental.id);
 
     if (url) {
-      const carUpdate = await this.carRepository.update(findPost.car.id, {
-        availability: false,
-      });
-      if (carUpdate.affected === 0)
-        throw new BadRequestException('Error al actualizar el vehiculo');
+      const carUpdate = await this.carRepository.update(findPost.car.id, { availability: false });
+      if (carUpdate.affected === 0) throw new BadRequestException('Error al actualizar el vehiculo');
       return url;
     } else {
       await this.rentalRepository.delete(rental.id);
@@ -115,27 +76,15 @@ export class RentalsService {
     // return await this.createWhithJWT(payload, rest, postId, payment);
   }
 
-  async createWhithJWT(
-    payload: JwtPayload,
-    rest: Partial<Rental>,
-    postId: string,
-    payment: Payment,
-  ) {
-    const rental_user = await this.userRepository.findOne({
-      where: { email: payload.sub },
-    });
+  async createWhithJWT( payload: JwtPayload, rest: Partial<Rental>, postId: string, payment: Payment ) {
+    const rental_user = await this.userRepository.findOne({ where: { email: payload.sub } });
 
     if (!rental_user) throw new NotFoundException('Usuario no encontrado');
 
     const newRental = this.rentalRepository.create(rest);
     if (!newRental)
-      throw new BadRequestException(
-        'Error al crear el contrato, verifique los datos',
-      );
-    const findPost = await this.postRepository.findOne({
-      where: { id: postId },
-      relations: ['car', 'user'],
-    });
+      throw new BadRequestException( 'Error al crear el contrato, verifique los datos' );
+    const findPost = await this.postRepository.findOne({ where: { id: postId }, relations: ['car', 'user'] });
     if (!findPost)
       throw new NotFoundException('Publicación no encontrada en la BD');
     if (findPost.user.email === rental_user.email)
@@ -169,27 +118,20 @@ export class RentalsService {
   }
 
   async findAll() {
-    const contracts = await this.rentalRepository.find({
-      relations: ['users', 'posts'],
-    });
+    const contracts = await this.rentalRepository.find({ relations: ['users', 'posts'] });
     if (!contracts)
       throw new NotFoundException('No hay contratos en la base de datos');
     return contracts;
   }
 
   async findOne(id: string) {
-    const contract = await this.rentalRepository.findOne({
-      where: { id },
-      relations: ['users'],
-    });
+    const contract = await this.rentalRepository.findOne({ where: { id }, relations: ['users'] });
     if (!contract) throw new NotFoundException('Contrato no encontrado');
     return contract;
   }
 
   async update(id: string, updateRentalDto: UpdateRentalDto) {
-    const contract = await this.rentalRepository.findOne({
-      where: { id },
-    });
+    const contract = await this.rentalRepository.findOne({ where: { id } });
     if (!contract) throw new NotFoundException('Contrato no encontrado');
     const updated = await this.rentalRepository.update(id, updateRentalDto);
     if (updated.affected === 0)
@@ -198,9 +140,7 @@ export class RentalsService {
   }
 
   async remove(id: string) {
-    const contract = await this.rentalRepository.findOne({
-      where: { id },
-    });
+    const contract = await this.rentalRepository.findOne({ where: { id } });
     if (!contract) throw new NotFoundException('Contrato no encontrado');
     const deleted = await this.rentalRepository.delete(id);
     if (deleted.affected === 0)
@@ -223,7 +163,6 @@ export class RentalsService {
               images: [dataPayment.image_url],
             },
             currency: 'usd',
-
             unit_amount: dataPayment.price * 100,
           },
           quantity: 1,
@@ -243,10 +182,7 @@ export class RentalsService {
   }
 
   async paymentSucess(id: string) {
-    const contract = await this.rentalRepository.findOne({
-      where: { id },
-      relations: ['users', 'posts', 'posts.user'],
-    });
+    const contract = await this.rentalRepository.findOne({ where: { id }, relations: ['users', 'posts', 'posts.user'] });
 
     console.log('Este es el contrato:', contract);
 
@@ -259,49 +195,27 @@ export class RentalsService {
 
     if (!userPay) throw new NotFoundException('Error al realizar el pago');
 
-    await this.notificationService.newNotification(
-      owner.email,
-      'rentedVehicle',
-      contractPost,
-    );
-
-    await this.notificationService.newNotification(
-      userPay[0].email,
-      'payConstancy',
-    );
-
+    await this.notificationService.newNotification( owner.email, 'rentedVehicle', contractPost );
+    await this.notificationService.newNotification( userPay[0].email, 'payConstancy' );
     return 'Compra pagada con exito';
   }
 
   async paymentCancel(id: string) {
-    const contract = await this.rentalRepository.findOne({
-      where: { id },
-      relations: ['users', 'posts', 'posts.user'],
-    });
+    const contract = await this.rentalRepository.findOne({ where: { id }, relations: ['users', 'posts', 'posts.user'] });
     if (!contract) throw new NotFoundException('Contrato no encontrado');
-    await this.carRepository.update(contract.posts.car.id, {
-      availability: true,
-    });
+    await this.carRepository.update(contract.posts.car.id, { availability: true });
     const deleted = await this.rentalRepository.delete(contract.id);
-    if (deleted.affected === 0)
-      throw new NotFoundException('Error al eliminar el contrato');
+    if (deleted.affected === 0) throw new NotFoundException('Error al eliminar el contrato');
     return 'Contrato cancelado con exito';
   }
   async getChat(token: string) {
     const secret = process.env.JWT_SECRET;
-    const payload: JwtPayload = await this.jwtService.verify(token, {
-      secret,
-    });
+    const payload: JwtPayload = await this.jwtService.verify(token, { secret });
     if (!payload) throw new UnauthorizedException('token invalido');
-    const rentals = await this.rentalRepository
-      .createQueryBuilder('rental')
-      .leftJoinAndSelect('rental.users', 'user')
-      .where('user.email = :email', { email: payload.sub })
+    const rentals = await this.rentalRepository.createQueryBuilder('rental').leftJoinAndSelect('rental.users', 'user').where('user.email = :email', { email: payload.sub })
       .getMany();
     if (rentals?.length === 0)
-      throw new NotFoundException(
-        'No hay registros de chats en la base de datos',
-      );
+      throw new NotFoundException( 'No hay registros de chats en la base de datos' );
     return rentals;
   }
 }
