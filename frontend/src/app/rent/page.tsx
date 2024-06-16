35ca04be620268b27751794f2ddb26b035f06785
Danilo Maccari <maccari78@gmail.com>
2024-06-16 13:07:32 -0300

    a little change

--- frontend/src/app/rent/page.tsx
+++ frontend/src/app/rent/page.tsx
@@ -129,9 +129,9 @@ if (loading) {
         </div>
       </div>
 
-      {/* Sección de estadísticas de ventas */}
+      {/* Sección de estadísticas de alquileres */}
       <div className="bg-[#333333] rounded-lg shadow-md p-6">
-        <h2 className="text-xl font-semibold text-[#C4FF0D]">Estadísticas de Ventas</h2>
+        <h2 className="text-xl font-semibold text-[#C4FF0D]">Estadísticas de Alquileres</h2>
         <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <StatCard
             title="Alquileres Totales"
@@ -170,7 +170,7 @@ const SaleCard: React.FC<SaleCardProps> = ({ carModel, saleDate, price, imageUrl
     <img className="w-full h-32 object-cover rounded-t-lg" src={imageUrl} alt={carModel} />
     <div className="mt-2">
       <h4 className="text-slate-100 font-bold text-lg">{carModel}</h4>
-      <p className="text-slate-400 text-sm mt-1">Fecha de venta: {saleDate}</p>
+      <p className="text-slate-400 text-sm mt-1">Fecha de alquiler: {saleDate}</p>
       <p className="text-gray-100 font-semibold mt-2">{price}</p>
     </div>
   </div>
