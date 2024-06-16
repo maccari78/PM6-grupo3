5bc252e37b7be9b62cbf1dbe6c82f233f35e8bd5
CAndresSierra <camilofullstack@gmail.com>
2024-06-15 22:19:26 -0500

    Mejora en el navbar

--- frontend/src/components/DropdownNavbar/Dropdown.tsx
+++ frontend/src/components/DropdownNavbar/Dropdown.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState, useEffect, useRef } from "react";
 import Swal from "sweetalert2";
+import { IUserDta } from "../Navbar/Navbar";
 
-const Dropdown: React.FC = () => {
+const Dropdown: React.FC<{
+  userDta: IUserDta | undefined;
+  loading: boolean;
+}> = ({ userDta, loading }) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -55,33 +59,130 @@ const Dropdown: React.FC = () => {
     };
   }, [isOpen]);
 
+  if (loading) {
+    return (
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="inline w-8 h-8 text-gray-200 animate-spin fill-[#C4FF0D]"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  }
+
   return (
-    <div className="relative inline-block text-left" ref={dropdownRef}>
+    <div
+      className="relative w-[170px] inline-block text-left"
+      ref={dropdownRef}
+    >
       <button
         onClick={toggleDropdown}
-        className="hidden md:inline-flex justify-center w-full px-4 py-2 text-sm font-medium  text-gray-300 bg-[#444343] rounded-md focus:outline-none"
+        className="hidden md:flex  flex-row gap-1 items-center justify-center w-full px-2 py-2 text-sm hover:underline hover:bg-[#444343] duration-200 rounded-md focus:outline-none"
       >
-        Mi cuenta
+        <div className="w-[20%]">
+          <img
+            src={userDta?.image_url}
+            alt="imagen de usuario"
+            className="rounded-full"
+          />
+        </div>
+        <p className="text-gray-200 text-[13px] ">{userDta?.name}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-5 h-4 stroke-gray-200"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 5l0 14" />
+          <path d="M18 13l-6 6" />
+          <path d="M6 13l6 6" />
+        </svg>
       </button>
+
       {isOpen && (
-        <div className="absolute z-50 right-0 w-56 mt-2 origin-top-right top-12 bg-[#222222] divide-y divide-gray-100 rounded-md shadow-lg">
+        <div className="absolute z-50 right-0 w-56 mt-2 origin-top-right top-12 bg-[#333333] divide-y divide-gray-100 rounded-md shadow-lg">
           <div className="py-1">
             <Link
               href="/user"
-              className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+              className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-5 h-5 stroke-[#C4FF0D]"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+              </svg>
               Mi cuenta
             </Link>
+
             <Link
               href="/vehicleForm"
-              className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+              className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] hover:stroke-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-5 h-5 stroke-[#C4FF0D]"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 21v-4a3 3 0 0 1 3 -3h5" />
+                <path d="M9 17l3 -3l-3 -3" />
+                <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                <path d="M5 11v-6a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-9.5" />
+              </svg>
               Publicar mi vehículo
             </Link>
             <Link
               href="/ayuda"
-              className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+              className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] hover:stroke-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-5 h-5 stroke-[#C4FF0D]"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                <path d="M12 17l0 .01" />
+                <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
+              </svg>
               Centro de ayuda
             </Link>
             <button
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { IUser } from "@/interfaces/IUser";
+
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -7,20 +7,64 @@ import { useEffect, useState, useRef } from "react";
 import Dropdown from "../DropdownNavbar/Dropdown";
 import Swal from "sweetalert2";
 
+const apiUser = process.env.NEXT_PUBLIC_API_GET_USERS_TOKEN;
+
+export interface IUserDta {
+  name: string;
+  image_url: string;
+}
+
 const Navbar: React.FC = () => {
   const pathname = usePathname();
   const router = useRouter();
 
   const [userSession, setUserSession] = useState<boolean>(false);
+  const [token, setToken] = useState<string | undefined>();
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [userDta, setUserDta] = useState<IUserDta | undefined>();
+  const [loading, setLoading] = useState<boolean>(false);
   const menuRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
+    if (
+      typeof window !== "undefined" &&
+      window.localStorage.getItem("userSession")
+    ) {
       const userToken = localStorage.getItem("userSession");
       setUserSession(!!userToken);
+      const parsedSession = JSON.parse(userToken!);
+      setToken(parsedSession.token!);
+    } else {
+      setUserSession(false);
     }
   }, [pathname]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (token) {
+        setLoading(true);
+        try {
+          const res = await fetch(`${apiUser}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token!}`,
+            },
+          });
+          const data: IUserDta = await res.json();
+          setUserDta(data);
+        } catch (error: any) {
+          console.log(error.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    if (userSession === true) {
+      fetchUser();
+    }
+  }, [token]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -67,7 +111,7 @@ const Navbar: React.FC = () => {
   }, [isMenuOpen]);
 
   return (
-    <header className="flex flex-row justify-between items-center bg-[#222222] text-white p-6 font-sans">
+    <header className="flex flex-row justify-around items-center bg-[#222222] text-white p-6 font-sans">
       <div className="flex flex-row gap-4 items-center">
         <Link href="/">
           <Image
@@ -79,18 +123,17 @@ const Navbar: React.FC = () => {
         </Link>
       </div>
       <div className=" md:w-[400px]">
-        <form className="flex items-center">
-          <div className="relative w-full">
+        <form className="flex items-center h-[30px] md:h-[42px]">
+          <div className="relative h-full  w-full">
             <input
               type="text"
               id="voice-search"
-              className="bg-gray-300 border rounded-l-2xl text-sm outline-none text-gray-900  focus:border-[#c2e94e] block w-full pl-10 p-2.5 placeholder-gray-500"
-              placeholder="vehiculo, Marca etc..."
-              required
+              className="bg-gray-300 h-full border rounded-l-2xl text-sm outline-none text-gray-900  focus:border-[#c2e94e] block w-full pl-3 p-2.5 placeholder-gray-500 placeholder:text-[10px] md:placeholder:text-[13px]"
+              placeholder="Modelo, Marca etc..."
             />
             <button
               type="reset"
-              className="flex absolute inset-y-0 right-0 items-center pr-3"
+              className="flex flex-row absolute inset-y-0 right-0 items-center pr-3 "
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -102,7 +145,7 @@ const Navbar: React.FC = () => {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="w-4 h-4 stroke-gray-800"
+                className="w-4 h-4 stroke-gray-800 hover:bg-gray-400 rounded-xl duration-200"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M18 6l-12 12" />
@@ -112,11 +155,11 @@ const Navbar: React.FC = () => {
           </div>
           <button
             type="submit"
-            className="inline-flex items-center rounded-r-2xl py-2.5 px-3  text-sm font-medium text-[#222222] bg-[#c2e94e] border border-[#c2e94e] hover:bg-[#94b338] focus:ring-4 focus:outline-none duration-300"
+            className="inline-flex h-full items-center  rounded-r-2xl py-2.5 px-3 hover:shadow-xl text-sm  bg-[#c2e94e] border border-[#c2e94e] hover:bg-[#94b338] focus:ring-4 focus:outline-none duration-300"
           >
             <svg
               aria-hidden="true"
-              className="mr-2 -ml-1 w-5 h-5"
+              className="md:mr-2 -ml-1 w-5 h-5 stroke-[#222222]"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -129,24 +172,24 @@ const Navbar: React.FC = () => {
                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               ></path>
             </svg>
-            Buscar
+            <p className="font-medium text-[#222222] hidden md:flex">Buscar</p>
           </button>
         </form>
       </div>
-      <div className="hidden md:flex flex-row items-center gap-8 font-medium">
+      <div className="hidden md:flex flex-row items-center  font-medium">
         {userSession ? (
-          <Dropdown />
+          <Dropdown userDta={userDta!} loading={loading} />
         ) : (
           <>
             <Link
               href="/login"
-              className="hover:text-[#C4FF0D] text-gray-300 duration-500"
+              className="hover:text-[#C4FF0D] text-gray-300 duration-500 pr-3 border-r-[2px] border-r-gray-300"
             >
               Iniciar sesion
             </Link>
             <Link
               href="/register"
-              className="hover:text-[#C4FF0D] text-gray-300 duration-500"
+              className="hover:text-[#C4FF0D] text-gray-300 duration-500 pl-3"
             >
               Registrarse
             </Link>
@@ -170,24 +213,71 @@ const Navbar: React.FC = () => {
       {isMenuOpen && (
         <div ref={menuRef}>
           {userSession ? (
-            <div className="absolute right-0 z-50 w-56 mt-2 origin-top-right top-16 bg-[#222222] divide-y divide-gray-100 rounded-md shadow-lg">
+            <div className="absolute right-0 z-50 w-56 mt-2 origin-top-right top-16 bg-[#333333] divide-y divide-gray-100 rounded-md shadow-lg">
               <div className="py-1">
                 <Link
                   href="/user"
-                  className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300  text-gray-300 hover:bg-[#494949] "
+                  className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5 stroke-[#C4FF0D]"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                    <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                  </svg>
                   Mi cuenta
                 </Link>
+
                 <Link
-                  href="/publicar-vehiculo"
-                  className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+                  href="/vehicleForm"
+                  className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] hover:stroke-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5 stroke-[#C4FF0D]"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M4 21v-4a3 3 0 0 1 3 -3h5" />
+                    <path d="M9 17l3 -3l-3 -3" />
+                    <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                    <path d="M5 11v-6a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-9.5" />
+                  </svg>
                   Publicar mi vehículo
                 </Link>
                 <Link
                   href="/ayuda"
-                  className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+                  className="flex items-center gap-1 flex-row px-4 py-2 text-sm hover:text-[#C4FF0D] hover:stroke-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5 stroke-[#C4FF0D]"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M12 17l0 .01" />
+                    <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
+                  </svg>
                   Centro de ayuda
                 </Link>
                 <button
@@ -219,14 +309,48 @@ const Navbar: React.FC = () => {
               <div className="py-1">
                 <Link
                   href="/login"
-                  className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949] "
+                  className="flex items-center gap-1 flex-row  px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949] "
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5 stroke-[#C4FF0D]"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9.615 20h-2.615a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8" />
+                    <path d="M14 19l2 2l4 -4" />
+                    <path d="M9 8h4" />
+                    <path d="M9 12h2" />
+                  </svg>
                   Iniciar sesion
                 </Link>
                 <Link
                   href="/register"
-                  className="block px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
+                  className="flex items-center gap-1 flex-row  px-4 py-2 text-sm hover:text-[#C4FF0D] duration-300 text-gray-300 hover:bg-[#494949]"
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-5 h-5 stroke-[#C4FF0D]"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3.5 5.5l1.5 1.5l2.5 -2.5" />
+                    <path d="M3.5 11.5l1.5 1.5l2.5 -2.5" />
+                    <path d="M3.5 17.5l1.5 1.5l2.5 -2.5" />
+                    <path d="M11 6l9 0" />
+                    <path d="M11 12l9 0" />
+                    <path d="M11 18l9 0" />
+                  </svg>
                   Registrarme
                 </Link>
               </div>
