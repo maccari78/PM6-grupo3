ac300ec3ded4f78b236f9abb0051d4f89e9c098a
Camila Fabre <camilafabre98@gmail.com>
2024-06-17 15:35:24 -0300

    loading en login y cambio de errores

--- frontend/src/helpers/validateLogin.ts
+++ frontend/src/helpers/validateLogin.ts
@@ -4,14 +4,14 @@ export function validateLogin(value:Ilogin):IErrorlogin {
     let errors:IErrorlogin = {}
         if(!value.email){
             setTimeout(() => {
-                errors.email = "El email es requiredo"
+                errors.email = "El email es requerido"
             }, 1000);
         } else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value.email)){
-            errors.email = "invalid email format"
+            errors.email = "Formato de email"
         }
         if (!value.password){
             setTimeout(() => {
-            errors.password = "La contraseña es requireda"
+            errors.password = "La contraseña es requerida"
         }, 1000);
         }
     return errors
