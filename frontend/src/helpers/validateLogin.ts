c2c233d3b78c703725ecc41ab41622a3319438c3
CAndresSierra <camilofullstack@gmail.com>
2024-06-18 18:14:19 -0500

    login renovado 2

--- frontend/src/helpers/validateLogin.ts
+++ frontend/src/helpers/validateLogin.ts
@@ -1,18 +1,16 @@
-import { IErrorlogin, Ilogin } from "@/interfaces/ILogin"
+import { IErrorlogin, Ilogin } from "@/interfaces/ILogin";
 
-export function validateLogin(value:Ilogin):IErrorlogin {
-    let errors:IErrorlogin = {}
-        if(!value.email){
-            setTimeout(() => {
-                errors.email = "El email es requerido"
-            }, 1000);
-        } else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value.email)){
-            errors.email = "Formato de email"
-        }
-        if (!value.password){
-            setTimeout(() => {
-            errors.password = "La contraseña es requerida"
-        }, 1000);
-        }
-    return errors
-    }
+export function validateLogin(value: Ilogin): IErrorlogin {
+  let errors: IErrorlogin = {};
+  if (!value.email) {
+    errors.email = "El email es requerido";
+  } else if (
+    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value.email)
+  ) {
+    errors.email = "Formato de email";
+  }
+  if (!value.password) {
+    errors.password = "La contraseña es requerida";
+  }
+  return errors;
+}
