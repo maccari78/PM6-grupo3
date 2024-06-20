1657355cb8f474c4a1a654f4ca0f70184f6f878c
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-06-20 00:23:27 -0300

    fix

--- frontend/src/components/admin/CarPostCard.tsx
+++ frontend/src/components/admin/CarPostCard.tsx
@@ -5,7 +5,6 @@ import { IPost } from '../VehiclesComponent/interfaces/IPost';
 import Swal from 'sweetalert2';
 import { redirect, useRouter } from 'next/navigation';
 import { RiCloseFill } from 'react-icons/ri';
-import UploadPost from '@/app/rent/[id]/page';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_POSTS;
 if (!apiUrl) {
